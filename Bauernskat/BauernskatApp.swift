6d949522a3c490785a75005bce7550bb90174c58
Darwin Schlenk <darwin.schlenk@stud.hs-hannover.de>
2023-02-04 21:03:10 +0100

    Initial Commit

---
+++ Bauernskat/BauernskatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BauernskatApp.swift
+//  Bauernskat
+//
+//  Created by Darwin Schlenk on 04.02.23.
+//
+
+import SwiftUI
+
+@main
+struct BauernskatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
